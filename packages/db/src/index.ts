0264dd425d2774f2874df9273137d6145fd437fc
painterpuppets <painterpuppets@gmail.com>
2025-12-22 10:34:10 +0900

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,12 @@
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { env } from "cloudflare:workers";
+import { drizzle } from "drizzle-orm/neon-http";
+import ws from "ws";
+
+import * as schema from "./schema";
+
+neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
+
+const sql = neon(env.DATABASE_URL || "");
+export const db = drizzle(sql, { schema });
